dfba0a8aefb21c1122a2a44ad1ec94868cc0f98d
ravipacharpro-jpg <ravipacharpro@gmail.com>
2025-11-28 18:14:39 +0530

    layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+// app/layout.tsx
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "HostGrow Panel",
+  description: "Serverless panel",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
