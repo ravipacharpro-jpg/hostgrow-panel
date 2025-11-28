c5c274c5322e6f75cfc1464a1fd61fcc7daa9f11
ravipacharpro-jpg <ravipacharpro@gmail.com>
2025-11-28 20:54:25 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
-// next.config.js
 const path = require('path');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  reactStrictMode: false,
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname), // "@/lib/..." => "<repo-root>/lib/..."
+      '@': path.resolve(__dirname),
     };
     return config;
   },
 };
-
-module.exports = nextConfig;
